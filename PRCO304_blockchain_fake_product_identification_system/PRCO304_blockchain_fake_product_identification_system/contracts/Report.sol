d2c0c08229b19aa7c0b68b97f3388bd1b974a047
AdamLi13 <62019246+AdamLi13@users.noreply.github.com>
2021-06-01 02:54:41 +0800

    archived the ver1 and updated the final version of the system

---
+++ PRCO304_blockchain_fake_product_identification_system/PRCO304_blockchain_fake_product_identification_system/contracts/Report.sol
@@ -0,0 +1,151 @@
+pragma solidity ^0.6.0;
+
+
+library Things{
+
+    struct Items {
+        string id; 
+        string name; 
+        string creator; 
+        // uint256 [] timestamps;
+        string ownerName; 
+        string ownerRole;
+        string customer; 
+        address [] track;
+        string [] track_role;
+        address owner; 
+        bool flag;
+    }
+
+    struct User{
+        string name; 
+        bool flag; 
+    }
+}
+
+contract Reports{
+    // string type manufacturer, distributor, retailer, customer;
+    using Things for Things.Items;
+    using Things for Things.User;
+    
+    mapping(string => mapping(address => Things.User)) register;
+    mapping(string => Things.Items) id2item;
+    mapping(address => bool) fake;
+
+    constructor() public
+    {
+
+    }
+
+    function blackUser(address send) public{
+        fake[send] = true;
+    }
+
+    function isBlackUser(address send) public returns (bool) {
+        return fake[send];
+    }
+
+    function isExist(address send, string memory role) public view returns (bool) {
+        return register[role][send].flag;
+    }
+
+    function unique(string memory id) public returns (bool){
+        return id2item[id].flag;
+    }
+
+    function addUser(address send, string memory name, string memory role) public returns (bool) {
+        if(isExist(send, role)){
+            return false;
+        }
+        register[role][send].name = name;
+        register[role][send].flag = true;
+        return true;
+    }
+    
+    // , uint256 timestamp
+    function addProduct(address send, string memory id, string memory name) public returns (bool){
+        string memory role = "manufacturer";
+        if(isExist(send, role) == false){
+            return false;
+        }
+
+        if(unique(id)){
+            return false;
+        }
+
+        Things.User memory holder = register[role][send]; 
+
+        id2item[id].id = id; 
+        id2item[id].name = name; 
+        id2item[id].creator = holder.name; 
+        // id2item[id].timestamps.push(timestamp); 
+        id2item[id].flag = true; 
+        id2item[id].ownerName = holder.name; 
+        id2item[id].ownerRole = role; 
+        id2item[id].owner = send; 
+        id2item[id].track.push(send);
+        id2item[id].track_role.push(role);
+        return true;
+    }
+
+    // , uint256 timestamp
+    function transfer(address send, string memory id, string memory role) public returns (bool){
+        if(isExist(send, role) == false){
+            return false;
+        }
+        if(unique(id) == false){
+            return false;
+        }
+        
+        Things.User memory holder = register[role][send]; 
+        // id2item[id].timestamps.push(timestamp); 
+        id2item[id].ownerName = holder.name; 
+        id2item[id].ownerRole = role; 
+        id2item[id].owner = send; 
+        id2item[id].track.push(send);
+        id2item[id].track_role.push(role);
+        return true;
+    }
+
+    function getItemById(string memory id) public returns (bool, string memory, string memory, string memory, string memory, string memory){
+        string memory ret = "";
+        if(unique(id) == false){
+            return (false, ret, ret, ret, ret, ret);
+        }
+        Things.Items memory item = id2item[id];
+        return (true, item.id, item.name, item.creator, item.ownerName, item.ownerRole);
+    }
+
+    function getTrackById(string memory id) public returns (bool, string memory, string memory){
+        uint i;
+        string memory ret = "";
+        string memory marks = " -> ";
+        if(unique(id) == false){
+            return (false, ret, ret);
+        }
+        Things.Items memory item = id2item[id];
+        for(i=0; i < item.track.length - 1; i++){
+            string memory role = item.track_role[i];
+            address send = item.track[i];
+            Things.User memory holder = register[role][send]; 
+            ret = string(abi.encodePacked(ret, holder.name, marks));
+        }
+        string memory role = item.track_role[i];
+        address send = item.track[i];
+        Things.User memory holder = register[role][send];
+        ret = string(abi.encodePacked(ret, holder.name));
+        return (true, ret, role);
+    }
+
+    function checkFake(string memory id) public returns (bool){
+        
+        if(unique(id) == false){
+            return true;
+        }
+        Things.Items memory item = id2item[id];
+        if(isExist(item.owner, item.ownerRole) == false){
+            return true;
+        }
+        return isBlackUser(item.owner);
+    }
+}
---
+++ PRCO304_blockchain_fake_product_identification_system/PRCO304_blockchain_fake_product_identification_system/scripts/test.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+
+from brownie import accounts
+from brownie import Reports
+import time
+import qrcode
+# import numpy as np
+# 10 accounts by default
+
+def main():
+    print("There are %d accounts."%(len(accounts)))
+
+    contracts = accounts[9].deploy(Reports)
+
+    roles = ["manufacturer", "distributor", "retailer", "customer"]
+    reviewers = ["A_manufacturer", "B_distributor", "C_retailer", "D_customer", "E_fake", "F_fake"]
+    goods = "shoes"
+
+    for i in [4, 5, 6, 7, 8]:
+        contracts.blackUser(accounts[i])
+
+    for i in range(len(roles)):
+        contracts.addUser(accounts[i], reviewers[i], roles[i])
+
+    for i in range(4, 5):
+        contracts.addUser(accounts[i], reviewers[i], "retailer")
+    
+    contracts.addProduct(accounts[0], "0001", goods)
+    contracts.addProduct(accounts[0], "0002", goods)
+    contracts.addProduct(accounts[0], "0003", goods)
+
+    tx = contracts.getItemById("0001")
+    print(tx.return_value)
+    tx = contracts.getItemById("0002")
+    print(tx.return_value)
+    tx = contracts.getItemById("0003")
+    print(tx.return_value)
+
+    contracts.transfer(accounts[1], "0001", "distributor")
+    tx = contracts.getItemById("0001")
+    print(tx.return_value)
+    tx = contracts.checkFake("0001")
+    if tx.return_value:
+        print("fake")
+    else:
+        print("goods")
+    contracts.transfer(accounts[2], "0001", "retailer")
+    tx = contracts.getItemById("0001")
+    print(tx.return_value)
+    tx = contracts.checkFake("0001")
+    if tx.return_value:
+        print("fake")
+    else:
+        print("goods")
+    contracts.transfer(accounts[3], "0001", "customer") # Buy it
+    tx = contracts.getItemById("0001")
+    print(tx.return_value)
+    # print(tx)
+    tx = contracts.checkFake("0001")
+    if tx.return_value:
+        print("fake")
+    else:
+        print("goods")
+
+    
+    if contracts.transfer(accounts[1], "0002", "retailer"):
+        print("fail to tansfer")
+
+    contracts.transfer(accounts[1], "0003", "distributor")
+    tx = contracts.getItemById("0003")
+    # print(tx.return_value)
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(str(tx.return_value))
+    qr.make(fit=True)
+    qr.print_ascii()
+    tx = contracts.checkFake("0003")
+    if tx.return_value:
+        print("fake")
+    else:
+        print("goods")
+    contracts.transfer(accounts[4], "0003", "retailer")
+    # tx = contracts.getItemById("0003")
+    tx = contracts.getTrackById("0003")
+    print(tx.return_value)
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(str(tx.return_value))
+    qr.make(fit=True)
+    qr.print_ascii()
+    tx = contracts.checkFake("0003")
+    if tx.return_value:
+        print("fake")
+    else:
+        print("goods")
+    # This is fake, do not buy it
+
+    time.sleep(3)
+
